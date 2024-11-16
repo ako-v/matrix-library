352530023e2cc9d98d9aca704f947629179d248d
Ako <development.front@trsystech.com>
2024-11-16 14:55:13 +0330

    feat: add radio group
    feat: add switch

---
+++ src/components/switch/Switch.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import * as React from "react";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
+
+import { cn } from "../../lib/utils";
+import { cva, VariantProps } from "class-variance-authority";
+
+const switchVariants = cva(
+  "peer inline-flex h-4 w-7 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+  {
+    variants: {
+      size: {
+        sm: "h-4 w-7 [&>span]:h-3 [&>span]:w-3 [&>span]:data-[state=checked]:translate-x-3",
+        md: "h-5 w-9 [&>span]:h-4 [&>span]:w-4 [&>span]:data-[state=checked]:translate-x-4",
+        lg: "h-6 w-11 [&>span]:h-5 [&>span]:w-5 [&>span]:data-[state=checked]:translate-x-5",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  },
+);
+
+interface SwitchProps extends SwitchPrimitives.SwitchProps, VariantProps<typeof switchVariants> {}
+
+const Switch = React.forwardRef<React.ElementRef<typeof SwitchPrimitives.Root>, SwitchProps>(({ className, size, ...props }, ref) => (
+  <SwitchPrimitives.Root className={cn(switchVariants({ size, className }))} {...props} ref={ref}>
+    <SwitchPrimitives.Thumb
+      className={cn(
+        "pointer-events-none block rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=unchecked]:translate-x-0",
+      )}
+    />
+  </SwitchPrimitives.Root>
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
+
+export { Switch };
